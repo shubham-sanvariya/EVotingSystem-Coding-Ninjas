03b8091a6d18c93750b3cb5b1bb30f1ce5c82c1f
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-07 21:33:06 +0530

    adding countByElection in vote repository

--- src/main/java/com/codingninjas/EVotingSystem/repositories/VoteRepository.java
+++ src/main/java/com/codingninjas/EVotingSystem/repositories/VoteRepository.java
@@ -2,8 +2,10 @@ package com.codingninjas.EVotingSystem.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.codingninjas.EVotingSystem.entities.Election;
 import com.codingninjas.EVotingSystem.entities.Vote;
 
 public interface VoteRepository extends JpaRepository<Vote,Long>{
     
+    Long countByElection(Election election);
 }
