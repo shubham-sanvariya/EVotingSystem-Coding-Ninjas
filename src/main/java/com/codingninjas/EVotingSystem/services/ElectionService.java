2461acfa27ee91906e701b0d8df6224e114b4639
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-07 15:24:28 +0530

    adding method addElection in ElectionService

--- src/main/java/com/codingninjas/EVotingSystem/services/ElectionService.java
+++ src/main/java/com/codingninjas/EVotingSystem/services/ElectionService.java
@@ -1,8 +1,17 @@
 package com.codingninjas.EVotingSystem.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.codingninjas.EVotingSystem.entities.Election;
+import com.codingninjas.EVotingSystem.repositories.ElectionRepository;
 
 @Service
 public class ElectionService {
-    
+    @Autowired
+    ElectionRepository electionRepository;
+
+    public void addElection(Election election){
+        electionRepository.save(election);
+    }
 }
