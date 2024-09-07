3bb5d64a9d31d1db43c082b29529584ec5793959
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-07 17:32:09 +0530

    adding method getElectionChoicesByElection in ElectionChoiceService

--- src/main/java/com/codingninjas/EVotingSystem/services/ElectionChoiceService.java
+++ src/main/java/com/codingninjas/EVotingSystem/services/ElectionChoiceService.java
@@ -1,12 +1,14 @@
 package com.codingninjas.EVotingSystem.services;
 
 import java.util.List;
-
+import java.util.NoSuchElementException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.codingninjas.EVotingSystem.entities.Election;
 import com.codingninjas.EVotingSystem.entities.ElectionChoice;
 import com.codingninjas.EVotingSystem.repositories.ElectionChoiceRepository;
+import com.codingninjas.EVotingSystem.repositories.ElectionRepository;
 
 @Service
 public class ElectionChoiceService {
@@ -14,11 +16,21 @@ public class ElectionChoiceService {
     @Autowired
     ElectionChoiceRepository repository;
 
+    @Autowired
+    ElectionRepository electionRepository;
+
     public void addElectionChoice(ElectionChoice electionChoice){
         repository.save(electionChoice);
     }
 
     public List<ElectionChoice> getAllElectionChoices(){
         return repository.findAll();
+    }
+
+    public List<ElectionChoice> getElectionChoicesByElection(Election election){
+        Election el = electionRepository.findById(election.getId())
+            .orElseThrow(() -> new NoSuchElementException("election id not found:"));
+            
+        return repository.findElectionChoicesByElection(el.getName());
     }
 }
