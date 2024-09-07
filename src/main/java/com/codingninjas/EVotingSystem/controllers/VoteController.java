205532ee9d4298c52b02a10e1991a67f78ae1aad
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-07 20:39:47 +0530

    adding add Vote method in Vote Controller

--- src/main/java/com/codingninjas/EVotingSystem/controllers/VoteController.java
+++ src/main/java/com/codingninjas/EVotingSystem/controllers/VoteController.java
@@ -1,8 +1,21 @@
 package com.codingninjas.EVotingSystem.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.codingninjas.EVotingSystem.entities.Vote;
+import com.codingninjas.EVotingSystem.services.VoteService;
 
 @RestController
 public class VoteController {
     
+    @Autowired
+    VoteService voteService;
+
+    @PostMapping("/add/vote")
+    public void addVote(@RequestBody Vote vote){
+        voteService.addVote(vote);
+    }
 }
