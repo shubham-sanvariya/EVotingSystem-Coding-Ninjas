025925026cd6d39ba091820b72795e3026fce746
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-07 15:25:00 +0530

    adding Election Controller

---
+++ src/main/java/com/codingninjas/EVotingSystem/controllers/ElectionController.java
@@ -0,0 +1,12 @@
+package com.codingninjas.EVotingSystem.controllers;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.codingninjas.EVotingSystem.entities.Election;
+
+@RestController
+public class ElectionController {
+    
+}
