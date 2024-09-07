abe04ebec4a11e8e176b308fb0ed3626c5ce5773
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-07 16:32:27 +0530

    adding method getAllElectionChoices in ElectionChoiceService

--- src/main/java/com/codingninjas/EVotingSystem/services/ElectionChoiceService.java
+++ src/main/java/com/codingninjas/EVotingSystem/services/ElectionChoiceService.java
@@ -1,5 +1,7 @@
 package com.codingninjas.EVotingSystem.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,5 +16,9 @@ public class ElectionChoiceService {
 
     public void addElectionChoice(ElectionChoice electionChoice){
         repository.save(electionChoice);
+    }
+
+    public List<ElectionChoice> getAllElectionChoices(){
+        return repository.findAll();
     }
 }
