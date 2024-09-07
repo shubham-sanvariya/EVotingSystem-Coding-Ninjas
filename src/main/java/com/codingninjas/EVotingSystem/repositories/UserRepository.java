17f472ead926eedc52b4d9eb9e4063beea4ef1ad
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-07 17:43:08 +0530

    adding User Repository

---
+++ src/main/java/com/codingninjas/EVotingSystem/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.codingninjas.EVotingSystem.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.codingninjas.EVotingSystem.entities.User;
+
+public interface UserRepository extends JpaRepository<User,Long>{
+    
+}
