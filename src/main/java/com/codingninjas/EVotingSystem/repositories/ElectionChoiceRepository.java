fb5715291ef17c33931ce65db14bf7ef3f13deb9
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-07 17:31:35 +0530

    adding findElectionChoicesByElection method in ElectionChoiceRepository

--- src/main/java/com/codingninjas/EVotingSystem/repositories/ElectionChoiceRepository.java
+++ src/main/java/com/codingninjas/EVotingSystem/repositories/ElectionChoiceRepository.java
@@ -1,9 +1,14 @@
 package com.codingninjas.EVotingSystem.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.codingninjas.EVotingSystem.entities.ElectionChoice;
 
 public interface ElectionChoiceRepository extends JpaRepository<ElectionChoice,Long>{
     
+    @Query("SELECT ec FROM ElectionChoice ec JOIN ec.election el WHERE el.name = :electionName")
+    List<ElectionChoice> findElectionChoicesByElection(String electionName);
 }
