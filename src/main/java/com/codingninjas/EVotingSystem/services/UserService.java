66f9da6ea837caa7396d4972971bbf5dc18ebd0c
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-07 17:49:16 +0530

    adding method getAllUsers in User Service

--- src/main/java/com/codingninjas/EVotingSystem/services/UserService.java
+++ src/main/java/com/codingninjas/EVotingSystem/services/UserService.java
@@ -1,5 +1,7 @@
 package com.codingninjas.EVotingSystem.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,5 +15,9 @@ public class UserService {
 
     public void addUser(User user){
         userRepository.save(user);
+    }
+
+    public List<User> getAllUsers(){
+        return userRepository.findAll();
     }
 }
