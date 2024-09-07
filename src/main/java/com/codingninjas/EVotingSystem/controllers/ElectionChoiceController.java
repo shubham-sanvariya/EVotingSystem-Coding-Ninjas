415d6eb951d0fb037064ab34e324916b161c959b
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-07 16:19:37 +0530

    adding method addElectionChoice in ElectionChoiceController

--- src/main/java/com/codingninjas/EVotingSystem/controllers/ElectionChoiceController.java
+++ src/main/java/com/codingninjas/EVotingSystem/controllers/ElectionChoiceController.java
@@ -1,8 +1,21 @@
 package com.codingninjas.EVotingSystem.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.codingninjas.EVotingSystem.entities.ElectionChoice;
+import com.codingninjas.EVotingSystem.services.ElectionChoiceService;
 
 @RestController
 public class ElectionChoiceController {
     
+    @Autowired
+    ElectionChoiceService service;
+
+    @PostMapping("/add/electionChoice")
+    public void addElectionChoice(@RequestBody ElectionChoice electionChoice){
+        service.addElectionChoice(electionChoice);
+    }
 }
