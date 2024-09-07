8df207d978b443a4b757b8ed196d32ac0935e215
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-07 20:37:55 +0530

    adding add Vote method in Vote Service

--- src/main/java/com/codingninjas/EVotingSystem/services/VoteService.java
+++ src/main/java/com/codingninjas/EVotingSystem/services/VoteService.java
@@ -1,8 +1,32 @@
 package com.codingninjas.EVotingSystem.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.codingninjas.EVotingSystem.entities.Vote;
+import com.codingninjas.EVotingSystem.repositories.VoteRepository;
 
 @Service
 public class VoteService {
     
+    @Autowired
+    VoteRepository voteRepository;
+
+    public void addVote(Vote vote){
+        if (vote == null) {
+            throw new IllegalArgumentException("Vote cannot be null");
+        }
+        if (vote.getUser() == null) {
+            throw new IllegalArgumentException("User cannot be null");
+        }
+        if (vote.getElection() == null) {
+            throw new IllegalArgumentException("Election cannot be null");
+        }
+        if (vote.getElectionChoice() == null) {
+            throw new IllegalArgumentException("Election choice cannot be null");
+        }
+
+        // Save the vote to the database
+        voteRepository.save(vote);
+    }
 }
