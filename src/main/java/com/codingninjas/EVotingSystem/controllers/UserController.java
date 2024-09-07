0bc5b3177e9c20ddf4cbf9df99ad9f5ba4f8ddde
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-07 17:48:20 +0530

    adding method add User in User Controller

--- src/main/java/com/codingninjas/EVotingSystem/controllers/UserController.java
+++ src/main/java/com/codingninjas/EVotingSystem/controllers/UserController.java
@@ -1,8 +1,21 @@
 package com.codingninjas.EVotingSystem.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.codingninjas.EVotingSystem.entities.User;
+import com.codingninjas.EVotingSystem.services.UserService;
 
 @RestController
 public class UserController {
     
+    @Autowired
+    UserService service;
+
+    @PostMapping("/add/user")
+    public void addUser(@RequestBody User user){
+        service.addUser(user);
+    }
 }
