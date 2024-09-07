1d134e29bfe9e1c5bbb137143f9226064388650b
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-07 17:50:31 +0530

    adding method getAllUsers in User Controller

--- src/main/java/com/codingninjas/EVotingSystem/controllers/UserController.java
+++ src/main/java/com/codingninjas/EVotingSystem/controllers/UserController.java
@@ -1,6 +1,9 @@
 package com.codingninjas.EVotingSystem.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,5 +20,10 @@ public class UserController {
     @PostMapping("/add/user")
     public void addUser(@RequestBody User user){
         service.addUser(user);
+    }
+
+    @GetMapping("/get/users")
+    public List<User> getAllUsers(){
+        return service.getAllUsers();
     }
 }
