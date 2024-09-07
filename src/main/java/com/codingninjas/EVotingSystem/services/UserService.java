b1a856abecd401f355af914367bcf4b513ac020d
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-07 17:46:34 +0530

    adding method add User in User Service

--- src/main/java/com/codingninjas/EVotingSystem/services/UserService.java
+++ src/main/java/com/codingninjas/EVotingSystem/services/UserService.java
@@ -1,8 +1,17 @@
 package com.codingninjas.EVotingSystem.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.codingninjas.EVotingSystem.entities.User;
+import com.codingninjas.EVotingSystem.repositories.UserRepository;
 
 @Service
 public class UserService {
-    
+    @Autowired
+    UserRepository userRepository;
+
+    public void addUser(User user){
+        userRepository.save(user);
+    }
 }
